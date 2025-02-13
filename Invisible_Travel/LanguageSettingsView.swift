3feb161df451ac1d8f7f30e7fb4d9985a83306a3
kch0i <kcchoi666@outlook.com>
2025-02-13 14:51:05 +0800

    1

--- Invisible_Travel/LanguageSettingsView.swift
+++
@@ -1,9 +0,0 @@
-//
-//  LanguageSettingsView.swift
-//  Invisible_Travel
-//
-//  Created by kc on 13/12/2024.
-//
-
-
-
--- Invisible_Travel/Menu.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Menu.swift
-//  Invisible_Travel
-//
-//  Created by kc on 10/12/2024.
-//
-
-
